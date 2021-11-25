afa9a44455ad4fc789522a3dfbe12be9ef9f6f46
durryx <giovanni3durante@gmail.com>
2021-11-25 23:08:17 +0100

    ...

--- uptest.sh
+++
@@ -1,4 +0,0 @@
-cp -rf .config vm-test
-cp -rf scripts vm-test
-cp -rf wallpaper vm-test
-cp -rf pkgs.csv addons.sh install.sh dotlist.csv local.conf .i3blocks.conf prew1.png prew2.png prew3.png prew4.png dance-doc.tex Video_Codec_SDK_11.0.10.zip profile-1.profile vm-test
